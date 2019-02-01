0a8398ef47821f977d8707cde3089375495f7c7d
ayhankuru <ayhankuru@yandex.com.tr>
2019-02-01 17:18:48 +0300

    init

---
+++ src/gatsby-browser.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import url from 'url'
+import { Minimatch } from 'minimatch'
+
+exports.onRenderBody = (
+  { setHeadComponents, pathname = `/` },
+  pluginOptions
+) => {
+  if (pluginOptions && pluginOptions.siteUrl) {
+    const parsedUrl = url.parse(pluginOptions.siteUrl)
+    const myUrl = `${pluginOptions.siteUrl}${pathname}`
+    if (
+      pluginOptions.excludedPaths.length > 0 &&
+      pathname &&
+      pluginOptions.excludedPaths.findIndex((_path) =>
+        new Minimatch(pathname).match(_path)
+      ) < 0
+    ) {
+      setHeadComponents([
+        <link
+          rel="canonical"
+          key={myUrl}
+          href={myUrl}
+          data-baseprotocol={parsedUrl.protocol}
+          data-basehost={parsedUrl.host}
+        />
+      ])
+    }
+  }
+}
